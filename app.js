57287cd40d9d4e0c39a260d8f6a45ed156a4e0b3
ludvikkemp <ludvik11@ru.is>
2017-11-28 08:37:21 +0000

    scraping for players and teams ready

--- app.js
+++ app.js
@@ -2,17 +2,14 @@ const express = require('express');
 const fs = require('fs');
 const request = require('request');
 const cheerio = require('cheerio');
-const rp = require('request-promise');
-const querystring = require('querystring');
 const bodyParser = require('body-parser');
 
 const app = express();
 app.use(bodyParser.json());
-const port = 8081;
 
 app.get('/scrape', (req, res) => {
-  const teamURL = 'https://www.transfermarkt.com/fc-liverpool/startseite/verein/31/saison_id/2017/';
   const leagueURL = 'https://www.transfermarkt.com/premier-league/startseite/wettbewerb/GB1';
+  const teamURL = 'https://www.transfermarkt.com/fc-liverpool/startseite/verein/31/saison_id/2017/';
   const playerURL = 'https://www.transfermarkt.com/thibaut-courtois/profil/spieler/108390';
   
   const form = {
@@ -27,34 +24,25 @@ app.get('/scrape', (req, res) => {
     'Content-Type' : 'application/x-www-form-urlencoded' 
   };
 
-  let players = [];
-  let teams = [];
-  let temp = []
-
-  request({ url: leagueURL, from: form, headers: headers }, (err, res, body) => {
+  request({ url: leagueURL, form: form, headers: headers }, (err, res, body) => {
     const $ = cheerio.load(body);
-    const data = $('.text-right.no-border-rechts.no-border-links').children().toArray();
-    
+    const data = $('.responsive-table .items tbody .zentriert .vereinprofil_tooltip').toArray();
+    const teams = [];
     for (let i = 0; i < data.length; i++) {
       const obj = {
-        id: data[i].attribs.id,
-        name: data[i].attribs.href.split('/',3)[1],
+        id: data[i].attribs.href.split('/', 7)[4],
+        name: data[i].attribs.href.split('/', 7)[1],
         href: data[i].attribs.href
       }
-      temp.push(obj);
+      teams.push(obj);
     }
-    // TODO: HERE I AM STUCK ATM!!!
-    // Duplicate teams in teams array
-    teams = temp.filter((item, pos) => {
-      return temp.indexOf(item) == pos;
-    })
-    console.log(teams.length)
+    //console.log(teams)
   })
 
-  request({ url: teamURL, from: form, headers: headers }, (err, res, body) => {
+  request({ url: teamURL, form: form, headers: headers }, (err, res, body) => {
     const $ = cheerio.load(body);
     const data = $('span.hide-for-small a.spielprofil_tooltip').toArray();
-    
+    const players = [];
     for (let i = 0; i < data.length; i++) {
       const obj =  {
         id: data[i].attribs.id,
@@ -63,20 +51,14 @@ app.get('/scrape', (req, res) => {
       }
       players.push(obj);
     }
-
-    //console.log(players);
-    /*
-    fs.writeFile('output.json', JSON.stringify(json, null, 4), function(err){
-      
-      console.log('File successfully written! - Check your project directory for the output.json file');
-      
-    })
-    */
+    //console.log(players)
   })
   
-  res.json(players);
+  res.json('Success');
 })
 
+const port = 8081;
 app.listen(port, () => console.log(`Shit is happening on port ${port}`));
 
 exports = module.exports = app;
+
