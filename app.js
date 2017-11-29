330c7c02433ad98559efd0bdd391ac0c351717e0
ludvikkemp <ludvik11@ru.is>
2017-11-29 00:59:35 +0000

    scraping split into 3 different get requests, more leagues added and the third data file leagues.js.

--- app.js
+++ app.js
@@ -7,76 +7,126 @@ const bodyParser = require('body-parser');
 const app = express();
 app.use(bodyParser.json());
 
-app.get('/scrape', (req, res) => {
-  const leagueURL = 'https://www.transfermarkt.com/premier-league/startseite/wettbewerb/GB1';
-  const teamURL = 'https://www.transfermarkt.com';
-  const playerURL = 'https://www.transfermarkt.com/thibaut-courtois/profil/spieler/108390';
-  
-  const form = { username: 'usr', password: 'pwd', opaque: 'opaque', logintype: '1' };
-  
-  const headers = { 
-    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:24.0) Gecko/20100101 Firefox/24.0',
-    'Content-Type' : 'application/x-www-form-urlencoded' 
-  };
+// Global Variables
+const europeURL = 'https://www.transfermarkt.com/wettbewerbe/europa';
+const mainURL = 'https://www.transfermarkt.com';
+let leagues = [];
+let teams = [];
+let players = [];
 
-  // ** Request numeber 1 **
-  // Scraping tansfermarket for list of team in the Premier Laeague
-  request({ url: leagueURL, form: form, headers: headers }, (err, res, body) => {
+// Form and headers for pretending to be a browser
+// in order to be able to scrape transfermarkt for data
+const form = { username: 'usr', password: 'pwd', opaque: 'opaque', logintype: '1' };
+const headers = { 
+  'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:24.0) Gecko/20100101 Firefox/24.0',
+  'Content-Type' : 'application/x-www-form-urlencoded' 
+};
+
+// *** 1.Scrape Leagues *** //
+app.get('/1', (req, res) => {
+  request({ url: europeURL, form: form, headers: headers }, (err, res, body) => {
     const $ = cheerio.load(body);
-    const data = $('.responsive-table .items tbody .zentriert .vereinprofil_tooltip').toArray();
-
-    const teamNames = $('.responsive-table .items tbody .hauptlink .vereinprofil_tooltip')
-                        .map(function(i, elem) { return $(this).text(); }).get()
-                        .join(', ').split(', ').filter((elem, i) =>  i % 2 === 0);
-
-    console.log(teamNames)
-
-    const teams = [];
-    for (let i = 0; i < data.length; i++) {
+    const leagueNames = $('.responsive-table .items tbody .hauptlink tr a')
+                          .toArray().filter((elem, i) => i % 2 !== 0);
+    const leagueNations = $('.responsive-table .items tbody .zentriert img').toArray()
+    
+    for (let i = 0; i < leagueNames.length; i++) {
       const obj = {
-        id: data[i].attribs.href.split('/', 7)[4],
-        name: teamNames[i],
-        href: data[i].attribs.href
+        id: leagueNames[i].attribs.href.split('/', 7)[4],
+        title: leagueNames[i].attribs.title,
+        nation: leagueNations[i].attribs.title,
+        href: leagueNames[i].attribs.href
       }
-      teams.push(obj);
+      leagues.push(obj);
     }
+  });
 
-    // ** Request numeber 2 **
-    // Within the first request in order to get url links to all teams in 
-    // the Premier League Loops through the teams array to get the links
-    let players = [];
-    for (let i = 0; i < teams.length; i++) {
-      request({ url: teamURL + teams[i].href, form: form, headers: headers }, (err, res, body) => {
-        const $ = cheerio.load(body);
-        const data = $('span.hide-for-small a.spielprofil_tooltip').toArray();
-        for (let j = 0; j < data.length; j++) {
-          const obj =  {
-            id: data[j].attribs.id,
-            name: data[j].attribs.title,
-            url: data[j].attribs.href,
-            team: {
-              id: teams[i].id,
-              name: teams[i].name
-            }
+  setTimeout(() => {
+    fs.writeFile('leagues.json', JSON.stringify(leagues, null, 4), (err) => {
+      console.log('leagues.json successfully written!');
+    })
+  }, 1000)
+
+  res.json({message:'Success'});
+})
+
+// *** 2.Scrape Teams *** //
+app.get('/2', (req, res) => {
+  if (leagues.length === 0) 
+    res.json({Error: 'You must call api/scrapeleagues first'}); 
+  
+  for (let i = 0; i < leagues.length; i++) {
+    request({ url: mainURL + leagues[i].href, form: form, headers: headers }, (err, res, body) => {
+      const $ = cheerio.load(body);
+      const data = $('.responsive-table .items tbody .zentriert .vereinprofil_tooltip').toArray();
+      const teamNames = $('.responsive-table .items tbody .hauptlink .vereinprofil_tooltip')
+                          .map(function(i, elem) { return $(this).text(); }).get()
+                          .join(', ').split(', ').filter((elem, i) =>  i % 2 === 0);
+      
+      for (let j = 0; j < data.length; j++) {
+        const obj = {
+          id: data[j].attribs.href.split('/', 7)[4],
+          name: teamNames[j],
+          href: data[j].attribs.href,
+          league: {
+            id: leagues[i].id,
+            title: leagues[i].title
           }
-          players.push(obj);
         }
-      })
-    }
+        teams.push(obj);
+      }
+    })
+    
+  }
 
-    // Timeout set to wait for all threads/callbacks to finish in request nr 2
-    // players.json and teams.json files created and data written to them
-    setTimeout(() => {
-      fs.writeFile('players.json', JSON.stringify(players, null, 4), (err) => {
-        console.log('players.json successfully written!');
-      })
-      fs.writeFile('teams.json', JSON.stringify(teams, null, 4), (err) => {
-        console.log('teams.json successfully written!');
-      })
-    }, 3000)
-  })
+  setTimeout(() => {
+    fs.writeFile('teams.json', JSON.stringify(teams, null, 4), (err) => {
+      console.log('teams.json successfully written!');
+    })
+  }, 12000)
+  
+  res.json({message:'Success'});
+})
 
-  res.json('Success');
+// *** 3.Scrape Players *** //
+app.get('/3', (req, res) => {
+  if (teams.length === 0) 
+    res.json({Error: 'You must call api/scrapeteams first'});
+  
+  for (let i = 0; i < teams.length; i++) {
+    request({ url: mainURL + teams[i].href, form: form, headers: headers }, (err, res, body) => {
+      console.log(mainURL + teams[i].href)
+      const $ = cheerio.load(body);
+      const data = $('span.hide-for-small a.spielprofil_tooltip').toArray();
+      
+      for (let j = 0; j < data.length; j++) {
+        const obj =  {
+          id: data[j].attribs.id,
+          name: data[j].attribs.title,
+          url: data[j].attribs.href,
+          team: {
+            id: teams[i].id,
+            name: teams[i].name
+          }
+        }
+        players.push(obj);
+      }
+    })
+  }
+
+  setTimeout(() => {
+    fs.writeFile('players.json', JSON.stringify(players, null, 4), (err) => {
+      console.log('players.json successfully written!');
+    })
+  }, 20000)
+  
+  res.json({message:'Success'});
+})
+
+// *** 4.Scrape Player Stats *** //
+app.get('4', (req, res) => {
+  // TODO: Scrape for player stats or Team stats
+  res.json({message:'Success'});
 })
 
 const port = 8081;
